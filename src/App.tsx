c0759a4e17582e465442d765e761aab18218225d
Ivan Stadnytskyy <vanst1@icloud.com>
2023-03-15 18:14:09 +0200

    Header start version

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import './App.scss';
+import { Header } from './modules/shared/Header';
 
 const App: React.FC = () => {
   return (
     <div className="App">
       <header className="App-header">
-        H1
+        <Header />
       </header>
     </div>
   );
