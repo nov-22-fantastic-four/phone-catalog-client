c87449f5b4cef465981a0227f7f38b0483bfdea1
Anastasiia Kravhenko <steys13712@gmail.com>
2023-03-16 18:30:03 +0200

    button

--- src/modules/shared/Footer/Footer.tsx
+++ src/modules/shared/Footer/Footer.tsx
@@ -2,7 +2,7 @@ import React, { useCallback } from 'react';
 import { Container } from '../Container';
 import styles from './Footer.module.scss';
 import logo from '../../../images/logo.svg';
-import vector from '../../../images/vector.svg';
+// import vector from '../../../images/vector.svg';
 
 export const Footer: React.FC = () => {
   const handleScrollToTop = useCallback(() => {
@@ -15,14 +15,14 @@ export const Footer: React.FC = () => {
   return (
     <Container>
       <div className={styles.footer}>
-      <div className={styles.logo_icon}>
-        <a href="#home">
-        <img
-        src={logo}
-        alt="logo"
-        className={styles.logo}
-        />
-      </a>
+        <div className={styles.logo_icon}>
+          <a href="#home">
+            <img
+              src={logo}
+              alt="logo"
+              className={styles.logo}
+            />
+          </a>
         </div>
 
         <nav className={styles.nav}>
@@ -44,7 +44,7 @@ export const Footer: React.FC = () => {
         <div className={styles.scroll}>
           <p className={styles.scroll_text}>Back to top</p>
           <button className={styles.scroll_button} onClick={handleScrollToTop}>
-            <img src={vector} alt="scroll to the top"/>
+            {/* <img src={vector} alt="scroll to the top"/> */}
           </button>
 
         </div>
