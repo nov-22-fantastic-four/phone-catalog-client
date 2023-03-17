d126fe7f42832835e8466ec0d36587a149b6f9b1
Olya Zaiets <zayets.olga@gmail.com>
2023-03-17 18:25:18 +0100

    added changes to footer

--- src/modules/shared/Footer/Footer.tsx
+++ src/modules/shared/Footer/Footer.tsx
@@ -1,8 +1,8 @@
 import React, { useCallback } from 'react';
-import { Container } from '../Container';
 import styles from './Footer.module.scss';
-import logo from '../../../images/logo.svg';
 import vector from '../../../images/icons/shevron-up.svg';
+import { LogoLink } from '../LogoLink';
+import { Container } from '../Container';
 
 export const Footer: React.FC = () => {
   const handleScrollToTop = useCallback(() => {
@@ -13,42 +13,49 @@ export const Footer: React.FC = () => {
   }, []);
 
   return (
-    <Container>
-      <div className={styles.footer}>
-        <div className={styles.logo_icon}>
-          <a href="#home">
-            <img
-              src={logo}
-              alt="logo"
-              className={styles.logo}
-            />
-          </a>
+
+    <footer className={styles.footer}>
+      <Container>
+        <div className={styles.content}>
+          <LogoLink className={styles.logo} />
+
+          <nav className={styles.nav}>
+            <ul className={styles.nav_list}>
+              <li className={styles.nav_item}>
+                <a
+                  className={styles.nav_link}
+                  href="https://github.com/nov-22-fantastic-four"
+                >
+                      Github
+                </a>
+              </li>
+
+              <li className={styles.nav_item}>
+                <a className={styles.nav_link} href="#contacts">Contacts</a>
+              </li>
+
+              <li className={styles.nav_item}>
+                <a className={styles.nav_link} href="#rights">Rights</a>
+              </li>
+            </ul>
+          </nav>
+
+          <div className={styles.scroll}>
+            <p className={styles.scroll_text}>Back to top</p>
+
+            <button
+              className={styles.scroll_button}
+              onClick={handleScrollToTop}
+            >
+              <img
+                src={vector}
+                className={styles.vector}
+                alt="scroll to the top"
+              />
+            </button>
+          </div>
         </div>
-
-        <nav className={styles.nav}>
-          <ul className={styles.nav_list}>
-            <li className={styles.nav_item}>
-              <a className={styles.nav_link} href="#github">Github</a>
-            </li>
-
-            <li className={styles.nav_item}>
-              <a className={styles.nav_link} href="#contacts">Contacts</a>
-            </li>
-
-            <li className={styles.nav_item}>
-              <a className={styles.nav_link} href="#rights">Rights</a>
-            </li>
-          </ul>
-        </nav>
-
-        <div className={styles.scroll}>
-          <p className={styles.scroll_text}>Back to top</p>
-
-          <button className={styles.scroll_button} onClick={handleScrollToTop}>
-            <img src={vector} alt="scroll to the top"/>
-          </button>
-        </div>
-      </div>
-    </Container>
+      </Container>
+    </footer>
   );
 };
