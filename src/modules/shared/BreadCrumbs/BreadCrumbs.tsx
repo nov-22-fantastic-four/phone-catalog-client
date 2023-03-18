abb72143c38e22feb4e102e275ad97faf4d839d8
Ivan Stadnytskyy <vanst1@icloud.com>
2023-03-18 12:38:06 +0200

    BreadCrumb added

--- src/modules/shared/BreadCrumbs/BreadCrumbs.tsx
+++ src/modules/shared/BreadCrumbs/BreadCrumbs.tsx
@@ -2,13 +2,16 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import styles from './BreadCrumbs.module.scss';
 import home_icon from '../../../images/icons/home_icon.svg';
+import shevron_r from '../../../images/icons/shevron-right.svg';
 import { useLocation } from 'react-router';
 
-export const BreadCrumbs: React.FC = () => {
-  const locationPathName = useLocation().pathname;
-  const locationArr = locationPathName.slice(1).split('/');
+interface Props {
+  phoneName?: string
+}
 
-  console.log(locationArr);
+export const BreadCrumbs: React.FC<Props> = ({ phoneName }) => {
+  const locationPathName = useLocation().pathname.slice(1).split('/')[0];
+  const bcName = locationPathName[0].toUpperCase() + locationPathName.slice(1);
 
   return (
     <div className={styles.bread_crumb}>
@@ -21,16 +24,27 @@ export const BreadCrumbs: React.FC = () => {
           alt='homeIcon'
           className={styles.icon} />
       </Link>
+      <img
+        src={shevron_r}
+        alt='homeIcon'
+        className={styles.shevron} />
 
-      {locationArr.map(page => (
-        <Link
-          key={page}
-          to={`/${page}`}
-          className={styles.link}
-        >
-          {`> ${page}`}
-        </Link>
-      ))}
+      {phoneName
+        ? <>
+          <Link
+            to={`/${locationPathName}`}
+            className={styles.link}
+          >
+            {bcName}
+          </Link>
+          <img
+            src={shevron_r}
+            alt='homeIcon'
+            className={styles.shevron} />
+          <span className={styles.noLink}>{phoneName}</span>
+        </>
+        : <span className={styles.noLink}>{bcName}</span>
+      }
     </div>
   );
 };
