a5a7514e97ed9ebfafcd5fc4b5c2f27c09b0b235
Ivan Stadnytskyy <vanst1@icloud.com>
2023-03-20 10:02:23 +0200

    Fix phone fetch path

--- src/modules/ProductPage/ProductPage.tsx
+++ src/modules/ProductPage/ProductPage.tsx
@@ -1,18 +1,20 @@
 import React, { useEffect, useState } from 'react';
+import { useLocation } from 'react-router';
 import { type Phone } from '../../types';
 import { BreadCrumbs, Container, BackButton } from '../shared';
 
 const samplePhoneLink =
-  'https://phone-catalog-api.onrender.com/phones/apple-iphone-7-32gb-black';
+  'https://phone-catalog-api.onrender.com/phones/';
 
 export const ProductPage: React.FC = () => {
   const [phone, setPhone] = useState<Phone | null>(null);
+  const location = useLocation().pathname.slice(8);
 
-  console.log(phone);
+  console.log(location);
 
   useEffect(() => {
     const fetchPhone = async(): Promise<void> => {
-      const res = await fetch(samplePhoneLink);
+      const res = await fetch(`${samplePhoneLink}/${location}`);
       const fetchedPhone = await res.json();
 
       setPhone(fetchedPhone);
