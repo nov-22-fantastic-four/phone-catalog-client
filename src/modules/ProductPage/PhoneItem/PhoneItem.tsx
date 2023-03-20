0a1b4ba4975fcfe20afa6eb4b3ab30ac40b19f53
Anastasiia Kravhenko <steys13712@gmail.com>
2023-03-20 15:45:20 +0200

    changed phoneInformation

--- src/modules/ProductPage/PhoneItem/PhoneItem.tsx
+++ src/modules/ProductPage/PhoneItem/PhoneItem.tsx
@@ -1,7 +1,6 @@
-import React, { useContext } from 'react';
+import React, { useContext, useState } from 'react';
 import { type Phone } from '../../../types';
 import { Image } from '../../shared/Image';
-// import { CartContext, FavoritesContext } from '../../../context';
 import cn from 'classnames';
 import styles from './PhoneItem.module.scss';
 import { PhoneInformation } from '../../shared/ProductCard/PhoneInformation';
@@ -14,11 +13,18 @@ interface Props {
 export const PhoneItem: React.FC<Props> = ({ phone }) => {
   const cartContext = useContext(CartContext);
   const favoritesContext = useContext(FavoritesContext);
-  //   const [selectedPhoto, setSelectedPhoto] = useState(phone?.images[0]);
 
   if (!phone) {
     return <p>Loading...</p>;
   }
+
+  const [selectedPhoto, setSelectedPhoto] = useState(phone.images[0]);
+  //   const [selectedColor, setSelectedColor]
+  //   = useState(phone.colorsAvailable[0]);
+
+  const handleSelectedPhoto = (photo: string): void => {
+    setSelectedPhoto(photo);
+  };
 
   const {
     id,
@@ -32,6 +38,7 @@ export const PhoneItem: React.FC<Props> = ({ phone }) => {
     capacity,
     camera,
     zoom,
+    cell,
   } = phone;
 
   const isAdded = cartContext.isAdded(+id);
@@ -70,7 +77,7 @@ export const PhoneItem: React.FC<Props> = ({ phone }) => {
                   src={photo}
                   alt={name}
                   className={styles.photo}
-                //   onClick={() => { handleClickAdded(photo); }}
+                  onClick={() => { handleSelectedPhoto(photo); }}
                 />
               </div>
             ))}
@@ -78,8 +85,8 @@ export const PhoneItem: React.FC<Props> = ({ phone }) => {
 
           <div className={styles.selectedPhoto}>
             <Image
-              src={phone?.images[0]}
-              alt={phone?.name}
+              src={selectedPhoto}
+              alt={name}
               className={styles.mainPhoto}
             />
           </div>
@@ -90,11 +97,12 @@ export const PhoneItem: React.FC<Props> = ({ phone }) => {
 
           <div className={styles.availableColor}>
             {phone?.colorsAvailable.map(currentColor => (
-              <button
-                key={id}
-                className={styles.color}
-                style={ { backgroundColor: currentColor }}
-              ></button>
+              <div key={id} className={styles.containerForColor}>
+                <div
+                  className={styles.color}
+                  style={ { backgroundColor: currentColor }}
+                ></div>
+              </div>
             ))}
           </div>
 
@@ -149,33 +157,25 @@ export const PhoneItem: React.FC<Props> = ({ phone }) => {
           </div>
 
           <div className={styles.phoneInfo}>
-            {phone && (
-              <PhoneInformation
-                text="Screen"
-                value={screen}
-              />
-            )}
+            <PhoneInformation
+              text="Screen"
+              value={screen}
+            />
 
-            {phone && (
-              <PhoneInformation
-                text="Resolution"
-                value={resolution}
-              />
-            )}
+            <PhoneInformation
+              text="Resolution"
+              value={resolution}
+            />
 
-            {phone && (
-              <PhoneInformation
-                text="Processor"
-                value={processor}
-              />
-            )}
+            <PhoneInformation
+              text="Processor"
+              value={processor}
+            />
 
-            {phone && (
-              <PhoneInformation
-                text="RAM"
-                value={ram}
-              />
-            )}
+            <PhoneInformation
+              text="RAM"
+              value={ram}
+            />
           </div>
         </div>
       </div>
@@ -185,6 +185,35 @@ export const PhoneItem: React.FC<Props> = ({ phone }) => {
           <h3 className={styles.titleInfo}>About</h3>
           <div className={styles.line}></div>
 
+          <div className={styles.textContainer}>
+            <h3 className={styles.textTitle}>And then there was Pro</h3>
+
+            <p className={styles.text}>A transformative triple‑camera system
+              that adds tons of capability without complexity.An unprecedented
+              leap in battery life. And a mind‑blowing chip that doubles down
+              on machine learning and pushes the boundaries of what a
+              smartphone can do. Welcome to the first iPhone powerful
+              enough to be called Pro.</p>
+
+            <h3 className={styles.textTitle}>Camera</h3>
+
+            <p className={styles.text}>Meet the first triple‑camera system to
+            combine cutting‑edge technology with the legendary simplicity of
+            iPhone. Capture up to four times more scene. Get beautiful images
+            in drastically lower light. Shoot the highest‑quality video in a
+            smartphone — then edit with the same tools you love for photos.
+            You’ve never shot with anything like it.</p>
+
+            <h3 className={styles.textTitle}>Shoot it. Flip it. Zoom it.
+              Crop it.Cut it. Light it. Tweak it. Love it.</h3>
+
+            <p className={styles.text}>iPhone 11 Pro lets you capture videos
+            that are beautifully true to life, with greater detail and smoother
+            motion. Epic processing power means it can shoot 4K video with
+            extended dynamic range and cinematic video stabilization — all at
+            60 fps. You get more creative control, too, with four times more
+            scene and powerful new editing tools to play with.</p>
+          </div>
         </div>
 
         <div className={styles.techSpecs}>
@@ -226,7 +255,11 @@ export const PhoneItem: React.FC<Props> = ({ phone }) => {
               text="Zoom"
               value={zoom}
             />
-            {/* capacity!!! */}
+
+            <PhoneInformation
+              text="Cell"
+              value={cell}
+            />
           </div>
         </div>
       </div>
